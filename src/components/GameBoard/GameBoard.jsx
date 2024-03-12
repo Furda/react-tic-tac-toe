e16e0b8d0bc19424acb39bc9e4e46d3e7f068927
Fernando <furdac14@gmail.com>
2024-03-12 22:18:47 +0100

    Add winning and draw functionality

---
+++ src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((palyerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={palyerSymbol !== null}
+                >
+                  {palyerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -11,6 +16,9 @@ export default function Player({ initialName, symbol }) {
 
   function toggleIsEditingHandler() {
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   let playerNameJSX = <span className="player-name">{playerName}</span>;
@@ -19,7 +27,7 @@ export default function Player({ initialName, symbol }) {
     playerNameJSX = (
       <input
         type="text"
-        requiered
+        required
         value={playerName}
         onChange={updateNameHandler}
       />
@@ -27,7 +35,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {playerNameJSX}
         <span className="player-symbol">{symbol}</span>
